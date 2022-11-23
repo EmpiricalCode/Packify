7980ae6bf20d45870581cc5b29d25f0407929847
EmpiricalCode <daniel.long@student.tdsb.on.ca>
2022-11-23 13:14:37 -0500

    Saving work to repo

--- core/handlers/loginWindowHandler.js
+++ core/handlers/loginWindowHandler.js
@@ -22,8 +22,8 @@ class LoginWindowHandler extends WindowHandler {
 
             // Spawn window
             this.window = WindowHandler.spawnWindow(path.join(__dirname, "../../public/html/login.html"), {
-                width: 300, 
-                height: 400,
+                width: 310, 
+                height: 480,
                 frame: false,
                 webPreferences : {
                     preload: path.join(__dirname, "../preloaders/loginPreload.js"),
